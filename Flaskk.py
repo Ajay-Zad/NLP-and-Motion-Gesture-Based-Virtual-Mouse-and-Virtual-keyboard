ec16b06eea34bb18219ea9c023a565cef6e01b66
Ajay_Zad18 <103515719+Ajay-Zad@users.noreply.github.com>
2023-08-12 01:10:16 +0530

    Add files via upload
    Added threading in the flask file

--- Flaskk.py
+++ Flaskk.py
@@ -9,6 +9,9 @@ from time import sleep
 import numpy as np
 import cvzone
 from pynput.keyboard import Controller
+import os
+import signal
+import threading
 
 app = Flask(__name__)
 
@@ -108,7 +111,7 @@ def empty():
 
         cv2.imshow('Virtual Mouse', frame)
         cv2.waitKey(1)
-    return render_template("empty.html")
+    return render_template("mouse_hand.html")
 
 @app.route("/empty1.html")
 def empty1():
@@ -148,79 +151,106 @@ def empty1():
                 
         cv2.imshow("HEAD controlled mouse",frame)
         cv2.waitKey(1)
-    return render_template("empty1.html")
+    return render_template("mouse_eye.html")
 
 @app.route("/empty2.html")
 def empty2():
     cam = cv2.VideoCapture(0)
     face_mesh = mp.solutions.face_mesh.FaceMesh(refine_landmarks=True)
+
+    # Get screen size
     screen_w, screen_h = pyautogui.size()
+
+    # Disable pyautogui FAILSAFE (be careful with this)
     pyautogui.FAILSAFE = False
-    while True:
-        _, frame = cam.read()
-        frame = cv2.flip(frame,1)
-        rgb_frame = cv2.cvtColor(frame,cv2.COLOR_BGR2RGB)
-        output = face_mesh.process(rgb_frame)
-        landmark_points = output.multi_face_landmarks
-        frame_h, frame_w, _ = frame.shape 
-        cnt = 0
-        
-        listener = sr.Recognizer()
-        try:
-            with sr.Microphone() as source:
-                print("LISTINING")       
-                listener.adjust_for_ambient_noise(source)
-                voice = listener.listen(source,.25,1)
-                command = str(listener.recognize_google(voice))
-                print(command)
-                c1 = command.lower()
-                print(c1)
-                if 'left' in c1 :
-                    print("LEFT ClICK")
-                    pyautogui.click()
-                elif 'right' in c1:
-                    print("RIGHT ClICK")
-                    pyautogui.click(button='right')
-                elif 'double' in c1 or 'click' in c1:
-                    print("DOUBLE ClICK")
-                    pyautogui.doubleClick()
-                elif 'down' in c1:
-                    print("SCROLLING DOWN")
-                    pyautogui.scroll(-1500)
-                elif 'up' in c1 or 'scroll' in c1:
-                    print("SCROLLING UP")
-                    pyautogui.scroll(1500)
-                elif 'start' in c1:
-                    print("WINDOWS START")
-                    pyautogui.click(0,1079)
-                elif 'close' in c1:
-                    print("CLOSING")
-                    pyautogui.click(1872,24)
-                elif 'min' in c1:
-                    print("MINIMIZE")
-                    pyautogui.click(1776,-29) 
-                elif 'add' in c1:
-                    print("ADDRESS BAR")
-                    pyautogui.click(258,55) 
-                else:
-                    pass
+
+    # Initialize Speech Recognizer
+    listener = sr.Recognizer()
+
+
+    def track_head():
+        while True:
+            _, frame = cam.read()
+            frame = cv2.flip(frame, 1)
+            rgb_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+            
+            # Process face mesh
+            output = face_mesh.process(rgb_frame)
+            landmark_points = output.multi_face_landmarks
+            
+            frame_h, frame_w, _ = frame.shape
+            
+            if landmark_points:
+                landmarks = landmark_points[0].landmark
+                for id, landmark in enumerate(landmarks[0:478]):
+                    x = int(landmark.x * frame_w)
+                    y = int(landmark.y * frame_h)
+                    cv2.circle(frame, (x, y), 3, (0, 255, 0))
                     
-        except:
-            pass
-        if landmark_points:
-            landmarks = landmark_points[0].landmark
-            for id,landmark in enumerate(landmarks[0:478]):
-                x = int(landmark.x * frame_w)
-                y = int(landmark.y * frame_h)
-                cv2.circle(frame,(x,y),3,(0,255,0))
-                if id == 1:
-                    screen_x = int(landmark.x * screen_w)
-                    screen_y = int(landmark.y * screen_h)
-                    pyautogui.moveTo(screen_x,screen_y)
-                    print("X = ",screen_x," Y = ",screen_y)
-        cv2.imshow("HEAD controlled mouse",frame)
-        cv2.waitKey(1)
-    return render_template("empty2.html")
+                    if id == 1:
+                        screen_x = int(landmark.x * screen_w)
+                        screen_y = int(landmark.y * screen_h)
+                        pyautogui.moveTo(screen_x, screen_y)
+                        
+            cv2.imshow("HEAD controlled mouse", frame)
+            cv2.waitKey(1)
+        
+    def recognize_speech():
+        while True:
+            try:
+                with sr.Microphone() as source:
+                    print("Listening...")
+                    listener.adjust_for_ambient_noise(source)
+                    voice = listener.listen(source, timeout=5, phrase_time_limit=1)
+                    command = str(listener.recognize_google(voice)).lower()
+                    print(command)
+                    
+                    if 'left' in command:
+                        print("Left click")
+                        pyautogui.click()
+                    elif 'right' in command:
+                        print("Right click")
+                        pyautogui.click(button='right')
+                    elif 'double' in command or 'click' in command:
+                        print("Double click")
+                        pyautogui.doubleClick()
+                    elif 'down' in command:
+                        print("Scrolling down")
+                        pyautogui.scroll(-1500)
+                    elif 'up' in command or 'scroll' in command or 'sc' in command or 'scr' in command:
+                        print("Scrolling up")
+                        pyautogui.scroll(1500)
+                    elif 'start' in command:
+                        print("Closing...")
+                        pyautogui.click(0, 1079)
+                    elif 'close' in command:
+                        print("Closing...")
+                        pyautogui.click(1872, 24)
+                    elif 'min' in command:
+                        print("Minimize")
+                        pyautogui.click(1776, -29)
+                    elif 'add' in command:
+                        print("Address bar")
+                        pyautogui.click(258, 55)
+                        
+            except sr.UnknownValueError:
+                pass
+            except sr.RequestError as e:
+                print(f"Error with the speech recognition service: {e}")
+            except Exception as e:
+                print(f"An error occurred: {e}")
+
+    # Start the head tracking thread
+    head_thread = threading.Thread(target=track_head)
+    head_thread.daemon = True
+    head_thread.start()
+
+    # Start the speech recognition thread
+    speech_thread = threading.Thread(target=recognize_speech)
+    speech_thread.daemon = True
+    speech_thread.start()
+
+    return render_template("mouse_voice.html")
 
 @app.route("/empty3.html")
 def empty3():
@@ -296,9 +326,16 @@ def empty3():
      
         cv2.imshow("Image", img)
         cv2.waitKey(1)
-    return render_template("empty3.html")
+    return render_template("keyboard_hand.html")
+
+@app.route("/shutdown.html")
+def shutdown():
+    print("Shutting down the server...")
+    os.kill(os.getpid(), signal.SIGINT)
+    return "Server shutting down..."
 
     
 
 if __name__ == "__main__":
     app.run(debug=True, port=5000)
+    
